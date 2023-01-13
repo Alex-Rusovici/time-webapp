58365ea03202a40a97b253cde828b0b6c749a13e
Alexandru Rusovici <alex.rusovici@yahoo.com>
2023-01-13 21:04:53 +0200

    structure - start
    structure - start

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+export default function Home() {
+    return (
+    <div>
+    
+    <h1>Hello world</h1>
+
+    <h2>Hello again</h2>
+
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit culpa officiis fuga. Voluptatem fuga rerum atque veniam impedit reiciendis tenetur praesentium laudantium voluptatibus, quam qui corporis, sapiente dolore quas veritatis.</p>
+    
+
+    <button className="button-primary">This is a button</button>
+    </div>
+    );
+}
